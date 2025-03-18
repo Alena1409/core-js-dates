488ff9c3003becbdcaf7def31a3adfc00d02a39c
Alena1409 <a77993519@gmail.com>
2025-03-18 12:52:53 +0400

    feat: add resolve of formatDate

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -174,8 +174,22 @@ function isDateInPeriod(date, period) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  const objData = new Date(date);
+  const year = objData.getUTCFullYear();
+  const month = objData.getUTCMonth() + 1;
+  const dates = objData.getUTCDate();
+  let hours = objData.getUTCHours();
+  const minutes = objData.getUTCMinutes().toString().padStart(2, '0');
+  const seconds = objData.getUTCSeconds().toString().padStart(2, '0');
+  const isAN = hours >= 12 ? 'PM' : 'AM';
+
+  if (hours === 0) {
+    hours = 12;
+  } else if (hours > 12) {
+    hours -= 12;
+  }
+  return `${month}/${dates}/${year}, ${hours}:${minutes}:${seconds} ${isAN}`;
 }
 
 /**
