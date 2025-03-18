638dabf68448a8b305217c24ac516622e5a3c3b4
Alena1409 <a77993519@gmail.com>
2025-03-18 16:56:35 +0400

    feat: add resolve of getNextFridayThe13th

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -257,8 +257,13 @@ function getWeekNumberByDate(date) {
  * Date(2024, 0, 13) => Date(2024, 8, 13)
  * Date(2023, 1, 1) => Date(2023, 9, 13)
  */
-function getNextFridayThe13th(/* date */) {
-  throw new Error('Not implemented');
+function getNextFridayThe13th(date) {
+  const newDate = new Date(date);
+  newDate.setUTCDate(12);
+  while (newDate.getDay() !== 5) {
+    newDate.setUTCMonth(newDate.getUTCMonth() + 1);
+  }
+  return newDate;
 }
 
 /**
